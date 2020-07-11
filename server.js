a431b292d14cdc353d3a6c99a38a8429bc8507f6
iamkhattar <shivamkhattar1@gmail.com>
2020-07-11 01:32:43 +0100

    Default Endpoint and User Endpoint Started

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Middleware Initialisation
+app.use(express.json({ extended: false }));
+
 // Default Endpoint
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.json({
     name: "BookiAPI",
     version: require("./package.json").version,
@@ -11,6 +14,9 @@ app.get("/", (req, res) => {
     date: new Date(),
   });
 });
+
+// Users Endpoint
+app.use("/api/users", require("./routes/api/users"));
 
 // Start Express Server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
