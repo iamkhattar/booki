c05f0af1f171b0dbd229a176d827a09ac4752ff7
John Lennox <jl1998@hotmail.co.uk>
2020-07-21 11:13:49 +0100

    created book schema

---
+++ models/Book.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const BookSchema = new mongoose.Schema({
+    isbn: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    reviews: [
+        {
+            message: {
+                type: String,
+            },
+            rating: {
+                type: Number,
+            }
+        },
+    ],
+});
+
+module.exports = Book = mongoose.model("book", UserSchema);
