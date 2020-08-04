b4e43182376880fa1e425b87be88f1321a8d8ba3
John Lennox <jl1998@hotmail.co.uk>
2020-08-04 15:23:43 +0100

    updating login page

--- webclient/src/components/Login/Components/WelcomeMessage.js
+++ webclient/src/components/Login/Components/WelcomeMessage.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "../Login.css";
-import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const WelcomeMessage = () => {
     return (
-        <div >
-            <Jumbotron>
-                <h1>Welcome back!</h1>
-                <br />
-                <p>
-                    Thank you for choosing Booki to facilitate your bookgroup needs.<br /> 
-                    Happy reading!
-  </p>
-            </Jumbotron>
+        <div className="jumbotron" >
+
+            <h1>Welcome back!</h1>
+            <br />
+            <p>
+                Thank you for choosing Booki to facilitate your bookgroup needs.<br />
+                Happy reading!
+            </p>
+
         </div>
     );
 };
--- webclient/src/components/Login/LoginPage.js
+++ webclient/src/components/Login/LoginPage.js
@@ -3,22 +3,25 @@ import "./Login.css";
 import LoginForm from "./Components/LoginForm";
 import WelcomeMessage from "./Components/WelcomeMessage";
 
+
 const LoginPage = () => {
   return (
     <div className="loginPage">
+      <div className="background-wrapper h-100 w-100">
+        <div className="background-image h-100 w-100"></div>
+      </div>
       <div className="container">
-        <div className="row">
-          <div className="col text-center heading">
-            <img width="40%" class = "img" src={require("../../assets/cover.png")} fluid />
+        <div className="row headerContent">
+          <div className="col text-center ">
+            <img width="50%"  className="logo m-2" src={require("../../assets/white-logo.png")} fluid />
+          
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-5">
-            <WelcomeMessage />
-          </div>
-          <div className="col-md-2"></div>
-          <div className="col-md-5">
-            <LoginForm />
+        <div className="content p-5">
+          <div className="row">
+            <div className="col-sm-12">
+              <LoginForm />
+            </div>
           </div>
         </div>
       </div>
